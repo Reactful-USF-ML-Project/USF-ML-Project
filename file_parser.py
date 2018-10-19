d7a77245f93d611abcb40c61d8e633df128cd909
Nick Perez <nperez0111@gmail.com>
2018-10-19 14:20:02 -0700

    made less code repetiition by using python modules

--- file_parser.py
+++ file_parser.py
@@ -16,97 +16,93 @@ def get_session_length(start, end):
     return (et - st).seconds
 
 def place_in_current_session(key,value,current_session,possible_values,map_to_session_index):
-    update_dict(possible_values, object_key, object_value)
+    update_dict(possible_values, key, value)
 
     if key == "page":
         current_session[map_to_session_index['page_count']] += 1
 
     elif key in map_to_session_index:
-        position_in_possibles = possible_values[object_key].index(value)
+        position_in_possibles = possible_values[key].index(value)
         current_session[map_to_session_index[key]] = position_in_possibles
 
 # Ordering of a session so far (used in map_to_session_index):
 # [ average time on page, region, type, device, page count, reaction combination, goal combination, session_length]
+def get_matrix():
+    with open('./results-20181008-130002 - results-20181008-130002.csv.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        session_ids = []
+        matrix = []
+        matrix_index = -1
+        start_time = -1
+        end_time = -1
+        possible_values = {}
+        map_to_session_index = {
+            'avg_time_per_page': 0,
+            'region': 1,
+            'type': 2,
+            'device':  3,
+            'page_count': 4,
+            'reaction': 5,
+            'goal': 6,
+            'session_length': 7
+        }
+        for row in csv_reader:
+            if line_count == 0:
+                labels = [] 
+                for l in row: 
+                    labels.append(l)
+            else:
+                sid = row[2]
 
-with open('./results-20181008-130002 - results-20181008-130002.csv.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    session_ids = []
-    matrix = []
-    matrix_index = -1
-    start_time = -1
-    end_time = -1
-    possible_values = {}
-    map_to_session_index = {
-        'avg_time_per_page': 0,
-        'region': 1,
-        'type': 2,
-        'device':  3,
-        'page_count': 4,
-        'reaction': 5,
-        'goal': 6,
-        'session_length': 7
-    }
-    for row in csv_reader:
-        if line_count == 0:
-            labels = [] 
-            for l in row: 
-                labels.append(l)
-        else:
-            sid = row[2]
+                if sid in session_ids:
+                    current_session = matrix[matrix_index]
+                    time = row[0]
+                    if time < start_time:
+                        start_time = time
+                    elif time > end_time:
+                        end_time = time
 
-            if sid in session_ids:
-                current_session = matrix[matrix_index]
-                time = row[0]
-                if time < start_time:
+                    object_key = row[3]
+                    object_value = row[4]
+                    place_in_current_session(object_key,object_value,current_session,possible_values,map_to_session_index)
+
+                else:
+                    if matrix_index > 0:
+                        last_session = matrix[matrix_index - 1]
+                        session_length = get_session_length(start_time, end_time)
+                        last_session[map_to_session_index['session_length']] = session_length
+                        page_count = last_session[map_to_session_index['page_count']]
+
+                        if session_length > 0 and page_count > 0:    
+                            last_session[map_to_session_index['avg_time_per_page']] = session_length / page_count
+                        else:
+                            last_session[map_to_session_index['avg_time_per_page']] = 0
+                    matrix_index += 1
+                    session_ids.append(sid)
+
+                    new_session = numpy.empty(len(map_to_session_index), dtype=object)
+                    matrix.append(new_session)
+                    # check if time is smaller than min or greater than max
+                    time = row[0]
                     start_time = time
-                elif time > end_time:
                     end_time = time
 
-                object_key = row[3]
-                object_value = row[4]
-                place_in_current_session(object_key,object_value,current_session,possible_values,map_to_session_index)
+                    current_session = matrix[matrix_index]
+                    current_session[map_to_session_index['reaction']] = 0 # Default reaction value of zero (temporary solution need something more robust)
+                    current_session[map_to_session_index['goal']] = 0 # Default goal value of zero (temporary solution need something more robust)
+                    current_session[map_to_session_index['page_count']] = 0 # Default page_count value of zero 
 
-            else:
-                if matrix_index > 0:
-                    last_session = matrix[matrix_index - 1]
-                    session_length = get_session_length(start_time, end_time)
-                    last_session[map_to_session_index['session_length']] = session_length
-                    page_count = last_session[map_to_session_index['page_count']]
+                    object_key = row[3]
+                    object_value = row[4]
+                    place_in_current_session(object_key,object_value,current_session,possible_values,map_to_session_index)
+     
+            line_count += 1
+        print("Processed " + str(line_count) + " lines.")
+        return matrix
 
-                    if session_length > 0 and page_count > 0:    
-                        last_session[map_to_session_index['avg_time_per_page']] = session_length / page_count
-                    else:
-                        last_session[map_to_session_index['avg_time_per_page']] = 0
-                matrix_index += 1
-                session_ids.append(sid)
-
-                a = numpy.empty(len(map_to_session_index), dtype=object)
-                matrix.append(a)
-                # check if time is smaller than min or greater than max
-                time = row[0]
-                start_time = time
-                end_time = time
-
-                # use object key val pair to
-                object_key = row[3]
-                object_value = row[4]
-                update_dict(possible_values, object_key, object_value)
-                current_session = matrix[matrix_index]
-                current_session[map_to_session_index['reaction']] = 0 # Default reaction value of zero
-                current_session[map_to_session_index['goal']] = 0 # Default goal value of zero 
-                current_session[map_to_session_index['page_count']] = 0 # Default page_count value of zero 
-                place_in_current_session(object_key,object_value,current_session,possible_values,map_to_session_index)
- 
-        line_count += 1
-    print("Processed " + str(line_count) + " lines.")
-    x = 0
-    # for e in possible_values:
-    #     print e
-    #     for x in possible_values[e]:
-    #         print "\t" + x
-    # print(matrix)
+if __name__ == "__main__": #If running the file on it's own just run the get_matrix() routine and print it
+    matrix = get_matrix()
     for a in matrix:
-            result = ", ".join(map(str, a))
-            print result
-    # print(labels)
+        result = ", ".join(map(str, a))
+        print result
