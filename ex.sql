038ed7b4b25032d90e4f08c80f291ba056c27c0c
Nick Perez <nperez0111@gmail.com>
2018-12-07 11:53:54 -0800

    mv stuff around

--- ex.sql
+++ ex.sql
@@ -1,34 +1,44 @@
 SELECT 
-  SessionId,
-  ANY_VALUE(type) as type,
-  ANY_VALUE(device) as device,
-  ANY_VALUE(region) as region,
-  SUM(page_count) as page_count,
-  MIN(UNIX_MILLIS(TIMESTAMP (CAST(date as STRING)))) as start_time,
-  MAX(UNIX_MILLIS(TIMESTAMP (CAST(date as STRING)))) as end_time
+    ANY_VALUE(type) as type,
+    ANY_VALUE(device) as device,
+    ANY_VALUE(region) as region,
+    STRING_AGG(reaction) as reaction,
+    SUM(page_count) as page_count,
+    MIN(UNIX_MILLIS(TIMESTAMP (CAST(date as STRING)))) as start_time,
+    MAX(UNIX_MILLIS(TIMESTAMP (CAST(date as STRING)))) as end_time
   FROM(
-  
-  SELECT
+    
+    SELECT
+      date,
+      ALL_SESSION_FOR_CLIENT.SessionId as SessionId,
+      (CASE WHEN ObjectType LIKE 'type' THEN ObjectId ELSE NULL END) as type,
+      (CASE WHEN ObjectType LIKE 'device' THEN ObjectId ELSE NULL END) as device,
+      (CASE WHEN ObjectType LIKE 'region' THEN ObjectId ELSE NULL END) as region,
+      (CASE WHEN ObjectType LIKE 'reaction' THEN ObjectId ELSE NULL END) as reaction,
+  --         subquery and join on date for the session, select date and get min and max for entire session ID        
+      COUNT(CASE WHEN ObjectType LIKE 'page' THEN 1 ELSE NULL END) as page_count
+
+        FROM (
+        SELECT
           date,
           SessionId,
-          (CASE WHEN ObjectType LIKE 'type' THEN ObjectId ELSE NULL END) as type,
-          (CASE WHEN ObjectType LIKE 'device' THEN ObjectId ELSE NULL END) as device,
-          (CASE WHEN ObjectType LIKE 'region' THEN ObjectId ELSE NULL END) as region,
-  --         subquery and join on date for the session, select date and get min and max for entire session ID        
-          COUNT(CASE WHEN ObjectType LIKE 'page' THEN 1 ELSE NULL END) as page_count
+          ObjectType,
+          ObjectId,
+          SourceType,
+          EventName
+        FROM usf_research.Informatica_Data_ALL_06012018_07312018) AS ALL_SESSION_FOR_CLIENT
+    --    Getting all reactions to JOIN upon
+        INNER JOIN (
+          SELECT SessionID 
+          FROM usf_research.Informatica_Data_ALL_06012018_07312018
 
-      FROM usf_research.Informatica_Data_ALL_06012018_07312018
---       WHERE SessionId = '03IbtwFMb4z5Z62DItC3Vl'
---       WHERE ((CASE WHEN ObjectType LIKE 'type' THEN ObjectId ELSE NULL END) IS NOT NULL
---       OR (CASE WHEN ObjectType LIKE 'device' THEN ObjectId ELSE NULL END) IS NOT NULL
---       OR (CASE WHEN ObjectType LIKE 'region' THEN ObjectId ELSE NULL END) IS NOT NULL
---       OR (CASE WHEN ObjectType LIKE 'page' THEN ObjectId ELSE NULL END) IS NOT NULL)
+          WHERE ObjectType = 'reaction' AND EventName like 'completed%'
+        ) AS GOAL_COMPLETED_SESSION
+    --    JOINing on reactions which have a completed reaction in their field by their SessionId
+        ON 
+        ALL_SESSION_FOR_CLIENT.SessionId = GOAL_COMPLETED_SESSION.SessionId
 
-      GROUP BY type, device, region, date, SessionId
---       LIMIT 12
-  )
---     WHERE SessionId = '03IbtwFMb4z5Z62DItC3Vl'
---     WHERE page_count != 0
-    GROUP BY SessionId
-    ORDER BY SessionId
-    LIMIT 10000
+        GROUP BY ALL_SESSION_FOR_CLIENT.SessionId,  ALL_SESSION_FOR_CLIENT.date, ALL_SESSION_FOR_CLIENT.EventName, ALL_SESSION_FOR_CLIENT.ObjectType, ALL_SESSION_FOR_CLIENT.ObjectId, ALL_SESSION_FOR_CLIENT.SourceType
+    )
+      GROUP BY SessionId
+      ORDER BY SessionId
--- test.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-import numpy as np
-import file_parser as matrix_generator
-feature_name_from_input_fn = 'test'
-vocabulary_feature_column = tf.feature_column.categorical_column_with_vocabulary_list( key=feature_name_from_input_fn, vocabulary_list=["kitchenware", "electronics", "sports"])
-print(vocabulary_feature_column)
